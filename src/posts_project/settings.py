bd959e31c7b9ea06aef5a606fd73a0fa82b0c07e
Victor Barbosa <victorbnogueira@gmail.com>
2024-04-01 17:12:46 -0400

    add installed apps

--- src/posts_project/settings.py
+++ src/posts_project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # our own apps
+    'posts',
+    'profiles',
+    # 3rd party apps
+    'cryspy_forms',
 ]
 
 MIDDLEWARE = [
