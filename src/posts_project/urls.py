53509786e60e3e027c068c35459186ea3434eac3
Victor Barbosa <victorbnogueira@gmail.com>
2024-04-03 14:49:19 -0400

    create main page and script file

--- src/posts_project/urls.py
+++ src/posts_project/urls.py
@@ -19,9 +19,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('posts.urls', namespace='posts'))
 ]
+
+
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
