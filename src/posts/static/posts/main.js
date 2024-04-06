965e9360adb77731ebe003f9ce3fc7dc11adf67f
Victor Barbosa <victorbnogueira@gmail.com>
2024-04-06 17:52:45 -0400

    reset form after submition

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -159,6 +159,7 @@ postForm.addEventListener('submit', e=> {
             likeUnlikePosts()
             $('#addPostModal').modal('hide')
             handleAlerts('success', 'New post added!')
+            postForm.reset()
         },
         error: function(error){
             console.log(error)
