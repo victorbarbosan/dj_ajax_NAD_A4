210098c534b93f550a8afda1c35f3e0db09dfb54
Victor Barbosa <victorbnogueira@gmail.com>
2024-04-06 19:50:20 -0400

    remove buttons if author is different

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -1,6 +1,8 @@
 console.log('hello world detail')
 
 const backBtn = document.getElementById('back-btn')
+const updateBtn = document.getElementById('update-btn')
+const deleteBtn = document.getElementById('delete-btn')
 const url = window.location.href + "data/"
 const spinnerBox = document.getElementById('spinner-box')
 
@@ -15,6 +17,18 @@ $.ajax({
     url: url,
     success: function(response){
         console.log(response)
+        const data = response.data
+
+        if (data.logged_in != data.author)
+        {
+            console.log('different')
+        } else {
+            console.log('the same')
+            updateBtn.classList.remove('not-visible')
+            deleteBtn.classList.remove('not-visible')
+        }
+
+
         spinnerBox.classList.add('not-visible')
     },
     error: function(error){
