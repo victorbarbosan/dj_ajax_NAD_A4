a4783daaaa0b9cdc3b9a441b16c85ee7d936e2c4
Victor Barbosa <victorbnogueira@gmail.com>
2024-04-06 20:28:32 -0400

    add delete functionality

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -93,3 +93,23 @@ updateForm.addEventListener('submit', e=> {
         }
     })
 })
+
+deleteForm.addEventListener('submit', e=>{
+    e.preventDefault()
+
+    $.ajax ({
+        type: 'POST',
+        url: deleteURL,
+        data: {
+            'csrfmiddlewaretoken': csrf[0].value,
+
+        },
+        success: function (response) {
+            window.location.href = window.location.origin
+            localStorage.setItem('title', titleInput.value)
+        },
+        error: function (error){
+            console.log(error)
+        }
+    })
+})
