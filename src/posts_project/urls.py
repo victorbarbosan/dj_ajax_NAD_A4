67142659a168924cb53df81876f0788ced66a653
Victor Barbosa <victorbnogueira@gmail.com>
2024-04-06 22:00:37 -0400

    create Profile page

--- src/posts_project/urls.py
+++ src/posts_project/urls.py
@@ -23,7 +23,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls', namespace='posts'))
+    path('', include('posts.urls', namespace='posts')),
+    path('profiles/', include('profiles.urls', namespace='profiles')),
 ]
 
 
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -16,7 +16,7 @@ def my_profile_view(request):
                 'bio': instance.bio,
                 'avatar': instance.avatar.url,
                 'user': instance.user.username,
-            })
+            }),
     context = {
         'obj': obj,
         'form': form,
